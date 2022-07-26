e3fe6881be160a520493b718325db1d1780a4cba
Rafsunjany_khan <rafsunjanykhan21@gmail.com>
2022-07-26 23:01:55 +0600

    search field done

--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.db.models import Q
 from .models import Student
-from django.db.models import q
+
 
 
 def student(request):
     if 'q' in request.GET:
         q = request.GET['q']
-        stu = Student.objects.filter((student_name__icontains=q)|(reg_name__icontains=q)|(father_name__icontains=q))
+        stu = Student.objects.filter(Q(student_name__icontains=q)|Q(reg_name__icontains=q)|
+                                     Q(student_roll_no__icontains=q)|Q(father_name__icontains=q)|
+                                     Q(mother_name__icontains=q)|Q(phone__icontains=q))
+
 
     else:
         stu = Student.objects.all()
